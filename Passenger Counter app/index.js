c42ac99c8f6d73a9c4bada9b9430d584d0121804
Manassingh610 <72378020+Manas610@users.noreply.github.com>
2023-03-15 23:54:37 +0530

    added functionality to save button

--- Passenger Counter app/index.js
+++ Passenger Counter app/index.js
@@ -1,24 +1,31 @@
-let x = document.getElementById("count-pass").innerHTML;
+let x = document.getElementById("count-pass").innerHTML
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-pass")
 
 function increment() {
-    x = parseInt(x) + 1;
-    document.getElementById("count-pass").innerHTML = x;
+    x = parseInt(x) + 1
+    countEl.innerHTML = x
 }
 
 function decrement(){
     if(x>0){
-    x = parseInt(x) - 1;
+    x = parseInt(x) - 1
     }
-    document.getElementById("count-pass").innerHTML = x;
+    countEl.innerHTML = x
 }
 
 function reset() {
     x = 0;
-    document.getElementById("count-pass").innerHTML = x;
+    countEl.innerHTML = x
+    saveEl.innerHTML = "Previous Entries:"
 }
 
 function save() {
-    
+    let countStr = x + " - "
+    //.textContent because we want spaces that are around " - " which will not be availble with .innerText
+    saveEl.textContent += countStr
+    x = 0
+    countEl.innerHTML = x
 }
 
 
