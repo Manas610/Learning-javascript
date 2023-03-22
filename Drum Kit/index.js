8ccecd972c5886ac7e0d3b92e4fd967b7da3a32b
Manas <singhmanas610@gmail.com>
2023-03-23 00:42:48 +0530

    Adding Javascript

---
+++ Drum Kit/index.js
@@ -0,0 +1,8 @@
+//let x = document.querySelector("button")
+
+for(var i =0 ; i < document.querySelectorAll(".drum").length ; i++){
+    document.querySelectorAll("button")[i].addEventListener("click" , function (){ 
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play()
+});
+};
