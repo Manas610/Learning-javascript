1421ddbac0319a92c421a65ba974543701442557
Manas <singhmanas610@gmail.com>
2023-04-05 21:23:46 +0530

    finished main working

--- Memory Game/js/index.js
+++ Memory Game/js/index.js
@@ -26,16 +26,95 @@ let cardsArray = [
 ];
 
 const parentDiv = document.querySelector('#card-section');
+let firstCard = "";
+let secondCard = "";
 
 const gameCard = cardsArray.concat(cardsArray);
 
-for(let i=0 ; i<gameCard.length ; i++ ){
+const shuffleFunction = (array) =>  {
+    for(let i = array.length - 1 ; i > 0 ; i-- ){
+        let j = Math.floor(Math.random() * (i + 1))
+        let temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+    }
+    return array
+} 
 
-    const childDiv = document.createElement('div');
-    childDiv.classList.add('card');
-    childDiv.dataset.name = gameCard[i].name;
-    childDiv.style.backgroundImage = `url(${gameCard[i].img})`
+const shuffleCard = shuffleFunction(gameCard)
 
-    parentDiv.appendChild(childDiv);
+
+
+const card_matches = () =>{
+    let card_selected = document.querySelectorAll('.card_selected')
+
+    card_selected.forEach((curEl) => {
+        curEl.classList.add('card_match')
+    })
+}
+
+const resetGame = () => {
+    firstCard = ""
+    secondCard = ""
+    clickCount = 0 
+    let card_selected = document.querySelectorAll('.card_selected')
+
+    card_selected.forEach((curEl) => {
+        curEl.classList.remove('card_selected')
+    })
+}
+
+
+let clickCount = 0
+
+parentDiv.addEventListener ('click' ,(event) => {
+    let currCard = event.target
+    if(currCard.id === "card-section"){return false}
+
+    clickCount++;
+    if(clickCount < 3){
+        if( clickCount === 1){
+            firstCard = currCard.parentNode.dataset.name;
+            currCard.parentNode.classList.add('card_selected')
+        } else {
+            secondCard = currCard.parentNode.dataset.name;
+            currCard.parentNode.classList.add('card_selected')
+        }
+
+        if(firstCard !== ""  && secondCard !== ""){
+            if(firstCard === secondCard){
+                //currCard.classList.add('card_match')
+                setTimeout( () => {
+                    card_matches()
+                    resetGame()
+                }, 1000)
+            } else {
+                setTimeout( () => {
+                    resetGame()
+                }, 1000)
+            }
+        }
+    } 
+
+})
+
+for(let i=0 ; i<shuffleCard.length ; i++ ){
+
+    const childDiv = document.createElement('div')
+    childDiv.classList.add('card')
+    childDiv.dataset.name = shuffleCard[i].name
+    //childDiv.style.backgroundImage = `url(${shuffleCard[i].img})`
+
+    const front_div = document.createElement('div')
+    front_div.classList.add('front-card')
+
+    const back_div = document.createElement('div')
+    back_div.classList.add('back-card')
+
+    back_div.style.backgroundImage = `url(${shuffleCard[i].img})`
+
+    parentDiv.appendChild(childDiv)
+    childDiv.appendChild(front_div)
+    childDiv.appendChild(back_div)
 
 }
