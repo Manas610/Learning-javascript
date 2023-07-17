d7935d62c826233a2da9507707fcd6d764dc9307
Manas <singhmanas610@gmail.com>
2023-07-17 22:43:55 +0530

    async await

---
+++ Javascript Theory/async-await.js
@@ -0,0 +1,17 @@
+// async function f(){
+//     let promise = new Promise((resolve, reject) => {
+//         //setTimeout(() => resolve("done"),1000);
+//         setTimeout(() => reject("oops"), 5000);
+//     });
+//     try{
+//         let result = await promise;
+//         console.log("Success");
+//     }
+//     catch (error){
+//         console.log(error);
+//     }
+// }
+
+// f();
+
+
