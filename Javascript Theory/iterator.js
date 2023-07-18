32e3a3ac34d9131307f9ba0713e1205ffd34b459
Manas <singhmanas610@gmail.com>
2023-07-18 23:46:05 +0530

    iterator

---
+++ Javascript Theory/iterator.js
@@ -0,0 +1,18 @@
+//Example - 1
+
+// var c = ["javascript","nodejs","reactjs","blockchain"];
+// let a = c[Symbol.iterator]();
+// let firstVal = a.next();
+// console.log(firstVal);
+
+
+//Example - 2
+
+// var a = "Hello";
+// let b = a[Symbol.iterator]();
+// let c= b.next();
+// while(!c.done){
+//     console.log(c.value);
+//     c = b.next();
+// }
+
