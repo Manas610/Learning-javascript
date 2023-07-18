caa1c5cf14583ef60e5aff1ead40fdd1d46c2a13
Manas <singhmanas610@gmail.com>
2023-07-19 00:03:50 +0530

    generator

---
+++ Javascript Theory/generator.js
@@ -0,0 +1,16 @@
+// function *Hello(){
+//     console.log("Hello");
+//     yield "first pause"
+//     console.log("How");
+//     yield "second pause"
+//     console.log("are");
+//     yield 10 + 20
+//     console.log("you");
+// }
+
+// let gen = Hello();
+// console.log(gen.next());
+// gen.next();
+// console.log(gen.next());
+// gen.next();
+
